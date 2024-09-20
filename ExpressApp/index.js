f7e6af00e4a7ade865ad3cf94280653d4bae6ea3
shiv-coder <shivi359@gmail.com>
2024-09-20 11:05:41 -0400

    Added connection string

--- ExpressApp/index.js
+++ ExpressApp/index.js
@@ -7,6 +7,9 @@ const mongoose = require('mongoose');
 //Initialize the express app
 const app = express();
 
+//MongoDb Atlas connection String
+const mongoURI = 'mongodb+srv://user2:user2@cluster0.lsiqq.mongodb.net/';
+
 //Middleware to parseJSON bodies
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
