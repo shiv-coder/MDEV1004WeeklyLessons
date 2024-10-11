cc814d4d9d2ca26180c7e46da047341e21c34ce4
shiv-coder <shivi359@gmail.com>
2024-10-11 11:00:30 -0400

    Updated index.js

--- ExpressApp/index.js
+++ ExpressApp/index.js
@@ -6,6 +6,11 @@ const movieRoutes = require('./src/routes/routes'); // Import the movie routes
 const {logger,handleNotFound} = require('./src/middleware/middleware');
 const fs = require('fs');
 dotenv.config({ path: './config.env' });
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const crypto = require('crypto');
+require('./src/config/passportConfig');
+const authRoutes = require('./src/routes/authRoutes');
 
 // Initialize MongoDB connection
 const InitiateMongoServer = require('./db');
@@ -13,6 +18,17 @@ InitiateMongoServer();
 
 // Initialize the express app
 const app = express();
+
+//Set us session and passport
+const secretKey = process.env.SECRET_KEY || crypto.randomBytes(64).toString('hex');
+console.log(`Generated Secret Key: ${secretKey}`);
+app.use(session({
+    secret:secretKey,
+    resave:false,
+    saveUninitialized:true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Read data from movies.json
 const data = JSON.parse(fs.readFileSync('./movies.json','utf-8'));
@@ -40,7 +56,8 @@ const importMovies = async (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
+//Use auth Routes
+app.use('/auth',authRoutes);
 // Define a root route
 app.get('/', (req, res) => {
     res.send('Welcome to the first program of Node.js Express');
