f6242ea8744a1cb92f080d73fab5a7349ecc61e8
shiv-coder <shivi359@gmail.com>
2024-11-01 09:46:13 -0400

    connected to database

--- jwtAuthentication/index.js
+++ jwtAuthentication/index.js
@@ -1,6 +1,8 @@
 const http = require('http');
 const routes = require('./routes/routes');
 const server = http.createServer(routes);
+require('dotenv').config();
+
 const { API_PORT} = process.env;
 const port = process.env.PORT || API_PORT;
 
