03484ccf304459ccc010eb0b360191da5aef2ba4
shiv-coder <shivi359@gmail.com>
2024-11-01 10:56:06 -0400

    Completed login route

--- jwtAuthentication/routes/routes.js
+++ jwtAuthentication/routes/routes.js
@@ -16,9 +16,9 @@ app.post('/register',async(req,res)=>{
         const { firstName,lastName,email,password}=req.body;
         if(!(email && password && firstName && lastName)){
             return res.status(400).send('All fields are required');
-        }
+        }//validation
 
-        const CheckUser = await User.findOne({email});
+        const CheckUser = await User.findOne({email});//checking for duplicacy
         if(CheckUser){
             return res.status(400).send('User is already registered');
         }
@@ -36,6 +36,7 @@ app.post('/register',async(req,res)=>{
                 expiresIn:'10m'}
             
         );
+        console.log(token);
         user.token = token;
         return res.status(201).json(user);
     }
@@ -43,4 +44,45 @@ app.post('/register',async(req,res)=>{
 console.log(err);
 return res.status(500).send('An error occured');
     }
-})
+});
+
+
+//Login route
+
+app.post('/login',async(req,res)=>{
+    try{
+        const { email,password}=req.body;
+        if(!(email && password )){
+            return res.status(400).send('All fields are required');
+        }//validation
+
+        const emailLower = email.toLowerCase();
+        const user = await User.findOne({email:emailLower});//checking for duplicacy
+        
+        //check user existence and compare the passwords
+        if(user && (await bcrypt.compare(password,user.password))){
+            const token = jwt.sign({
+                user_id:user._id,email},
+                process.env.TOKEN_KEY,{
+                    expiresIn:'10m'}
+                
+            );
+            user.token = token;
+            return res.status(201).json(user);
+
+        } else{
+            console.log('Invalid credentials');
+        }
+        return res.status(400).send('Invalid credentials');
+   
+           
+    }
+    catch(err){
+console.log('Error during login',err);
+return res.status(500).send('An error occured');
+    }
+});
+
+
+module.exports = app;
+
