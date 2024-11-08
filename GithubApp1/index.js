19bfdb87e2bf8a2bbf6b9a8d70503359e4bf759b
shiv-coder <shivi359@gmail.com>
2024-11-08 10:09:35 -0500

    Completed index.js file

---
+++ GithubApp1/index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const { getRepositories} = require('./controllers/repoController');
+
+const app = express();
+const PORT = process.env.PORT ||3000;
+
+//connect to MONGODB
+
+mongoose
+.connect(process.env.MONGO_URI)
+.then(()=>console.log('Connected to MongoDB'))
+.catch((error)=>console.log('MongoDB Connection error',error));
+
+
+//Define routes
+app.get('/fetch-repositories',getRepositories);
+
+//Start server
+app.listen(PORT,()=>{
+    console.log(`Server running on http://localhost:${PORT}`);
+});
